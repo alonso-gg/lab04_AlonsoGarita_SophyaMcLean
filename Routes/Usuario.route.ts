59871e5b69faf9317624e36de583a3a2b1048796
sophMcMor <sophyafmm@estudiantec.cr>
2024-01-03 13:10:27 -0600

    Usuarios listos y tanteo con middleware

--- Routes/Usuario.route.ts
+++ Routes/Usuario.route.ts
@@ -16,4 +16,32 @@ UsuarioRouter.post('/', async (req, res) => {
     res.status(201).json(newUser);
 });
 
+//Modifica UN usuario por su username (nombre)
+UsuarioRouter.put('/:nombre',async (req,res) => {
+    const {nombre} = req.params; //Obtiene el username que le enviaron por parámetros
+    const usuarioBuscado = await ModelUsuario.find({nombre}).lean().exec(); 
+    if (usuarioBuscado.length === 0) {
+        res.status(404).json({Mensaje: `No hay usuarios registrados con el nombre ${nombre}` });
+    }
+    else {
+        await ModelUsuario.updateOne({ nombre: nombre }, { $set: { contrasena: req.body.contrasena } });
+        res.status(202).json({Mensaje: `El usuario ${nombre} fue modificado exitosamente.` });
+    }    
+})
+
+// Es un servicio para borrar un usuario
+UsuarioRouter.delete('/:nombre', async (req, res) => {
+    const {nombre} = req.params; //Obtiene el username que le enviaron por parámetros
+    const usuarioBuscado = await ModelUsuario.find({nombre}).lean().exec(); 
+    if (usuarioBuscado.length === 0) {
+        res.status(404).json({Mensaje: `No hay usuarios registrados con el nombre ${nombre}` });
+    }
+    else {
+        await ModelUsuario.deleteOne({ nombre: nombre }, { $set: { contrasena: req.body.contrasena } });
+        res.status(202).json({Mensaje: `El usuario ${nombre} se eliminó con éxito!` });
+    }  
+})
+
+
+
 export default UsuarioRouter;
--- index.ts
+++ index.ts
@@ -1,10 +1,19 @@
 import mongoose, { Schema } from "mongoose";
 import express = require("express");
+import { Request, Response, NextFunction } from 'express'; //agregado para el middleware
 import UsuarioRouter from "./Routes/Usuario.route.ts";
 import HabilidadesRouter from "./Routes/Habilidades.route.ts";
 
 const app = express();
 const port = 3000;
+const authenticationMiddleware = (req: Request, result: Response, next: () => any) => {
+    if (req.headers.authorization === 'Basic andres:obando') {
+        next();
+    }
+    else {
+        return result.status(401).json({ message: 'El usuario no esta autorizado' });
+    }
+}
 app.use(express.json());
 
 app.use('/users', UsuarioRouter);
